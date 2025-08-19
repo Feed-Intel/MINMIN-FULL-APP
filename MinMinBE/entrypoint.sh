020301f42dc11db7d91a1f52db5277bebd4490c2
Feed-Intel <hailemichael@feed-intel.com>
2025-08-19 15:03:42 +0300

    fix:deployment

--- MinMinBE/entrypoint.sh
+++ MinMinBE/entrypoint.sh
@@ -49,7 +49,6 @@ python manage.py migrate --noinput
 echo "Collecting static..."
 python manage.py collectstatic --noinput 
 
-
 echo "Ensuring superuser..."
 python manage.py shell <<'EOF'
 from django.contrib.auth import get_user_model
