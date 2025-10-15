b90ce7e6cd18daa077f572509d396ea250995fea
Dawit Hailu <Dawit.h@feed-intel.com>
2025-10-15 12:47:21 +0300

    fix: add GOOGLE Client ID and Secret to api.env

--- customerFE/app/api/auth/token+api.ts
+++ customerFE/app/api/auth/token+api.ts
@@ -3,43 +3,44 @@ import {
   GOOGLE_CLIENT_SECRET,
   GOOGLE_REDIRECT_URI,
   API_KEY,
-} from "@/utils/constants";
+} from '@/utils/constants';
 
 export async function POST(request: Request) {
   const body = await request.formData();
-  const code = body.get("code") as string;
+  const code = body.get('code') as string;
   if (!code) {
     return Response.json(
-      { error: "Missing authorization code" },
+      { error: 'Missing authorization code' },
       { status: 400 }
     );
   }
-  const response = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  const response = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams({
       client_id: GOOGLE_CLIENT_ID,
       client_secret: GOOGLE_CLIENT_SECRET,
       redirect_uri: GOOGLE_REDIRECT_URI,
-      grant_type: "authorization_code",
+      grant_type: 'authorization_code',
       code: code,
     }),
   });
 
   const data = await response.json();
+  console.log(data.id_token);
   if (!data.id_token) {
     return Response.json(
-      { error: "Missing required parameters" },
+      { error: 'Missing required parameters' },
       { status: 400 }
     );
   }
   const resp = await fetch(
     `${process.env.EXPO_PUBLIC_BACKEND_URL}/auth/social/google/`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "X-API-KEY": API_KEY,
+        'Content-Type': 'application/json',
+        'X-API-KEY': API_KEY,
       },
       body: JSON.stringify({
         id_token: data.id_token,
