641235d8b4a5108c0bcb61498d2e37b827d76592
Feed-Intel <hailemichael@feed-intel.com>
2025-11-02 12:12:16 +0300

    Delete MinMinBENode directory

--- MinMinBENode/src/modules/ai/dto/run-inference.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsObject, IsString } from 'class-validator';
-
-export class RunInferenceDto {
-  @IsString()
-  model!: string;
-
-  @IsObject()
-  payload!: Record<string, unknown>;
-}
--- MinMinBENode/src/modules/loyalty/dto/update-points.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsString } from 'class-validator';
-
-export class UpdatePointsDto {
-  @IsString()
-  accountId!: string;
-
-  @IsNumber()
-  delta!: number;
-}
